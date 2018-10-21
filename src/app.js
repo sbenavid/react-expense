1a48199b34fe8dbf84fed28ddfc1d9d9773c4795
sbenavid <sbenavid@gmail.com>
2018-10-21 11:51:04 -0500

    Agrego navegacion simple usando NavLink

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // React-route
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 // estilos
 import 'normalize.css/normalize.css' // normalize es para dar imagen unificada entre browsers
 import './styles/styles.scss'
@@ -30,20 +30,40 @@ const HelpExpensePage = () => (
     </div>
 );
 
+// Link esta usando enlaces "client-side"
 const NotFoundPage = () => (
     <div>
-        <p>404!</p>
+        404! - <Link to="/">Regresar al inicio</Link>
     </div>
 );
+
+// definir componente que estara en todas las paginas
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        {/* link to home page */}
+        <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
+        {/* Link to create page */}
+        <NavLink to="/create" activeClassName="is-active">Nuevo</NavLink>
+        {/* link to edit page */}
+        <NavLink to="/edit" activeClassName="is-active">Cambiar</NavLink>
+        {/* link to help page */}
+        <NavLink to="/help" activeClassName="is-active">Ayuda</NavLink>
+    </header>
+);
+
 const routes = (
     <BrowserRouter>
-        <Switch>
+        <div>
+            <Header />
+            <Switch>
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage} />
             <Route path="/edit" component={EditExpensePage} />
             <Route path="/help" component={HelpExpensePage} />
             <Route component={NotFoundPage} />
-        </Switch>        
+        </Switch>  
+        </div>              
     </BrowserRouter>
 );
   
