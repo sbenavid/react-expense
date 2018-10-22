08acd5306b05c0961210fa7d70b5ff1b27f2ea10
sbenavid <sbenavid@gmail.com>
2018-10-21 22:42:15 -0500

    cambio de config en app principal para empezar a tomar todos los componentes

--- src/app.js
+++ src/app.js
@@ -5,5 +5,10 @@ import 'normalize.css/normalize.css' // normalize es para dar imagen unificada e
 import './styles/styles.scss'
 // componentes creados
 import AppRouter from './routers/AppRouter';
+import configStore from './store/configStore';
+
+const store = configStore();
+
+console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
