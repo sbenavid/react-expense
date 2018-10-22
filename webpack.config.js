766fbd793395b8f591af6a889f2ca15b67137eec
sbenavid <sbenavid@gmail.com>
2018-10-21 22:38:39 -0500

    se regresa config. a archivo principal de app

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 // formato
 // entry -> output
 module.exports = {
-  entry: './src/playground/redux-expensify.js',
+  entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
