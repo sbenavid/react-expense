1ef9633ce6cc180d64139640dad08e9cd6862b5c
sbenavid <sbenavid@gmail.com>
2018-10-23 17:33:21 -0500

    Integro nuevos elementos al dashboard

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseListFilters from './ExpenseListFilters';
 
 const ExpenseDashboardPage = () => (
     <div>
         <p>Esto es el copmonente de dashboard...</p>
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
