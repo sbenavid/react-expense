2e898eaa79f74770317028fcb9e6a9b28e79ec88
sbenavid <sbenavid@gmail.com>
2018-10-23 17:33:57 -0500

    quitar lineas en blanco

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
+
 // export a stateless functional component
 // description, amount, createdAt
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <h3>{description}</h3>
         <p>{amount}-{createdAt}</p>
+        <button onClick={() =>{
+            dispatch(removeExpense({ id }));
+        }}>Quitar</button>
     </div>
 );
 
-
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
