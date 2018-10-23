a689aa15bb551c31438ba0721b107d4a099898c3
sbenavid <sbenavid@gmail.com>
2018-10-23 00:48:19 -0500

    primeras pruebas de usar react-redux

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 // estilos
 import 'normalize.css/normalize.css' // normalize es para dar imagen unificada entre browsers
 import './styles/styles.scss'
@@ -16,12 +17,22 @@ const store = configStore();
 store.dispatch(addExpense({ description: 'recibo del agua' }));
 store.dispatch(addExpense({ description: 'recibo gas natural' }));
 // filtro por "recibo"
-store.dispatch(setTextFiler('gas'));
+store.dispatch(setTextFiler('recibo'));
+
+// funcion para hacer un timeout
+setTimeout(() => {
+    store.dispatch(setTextFiler('gas'));
+},3000);
 
 // definir 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ExpenseList from './ExpenseList';
 
 const ExpenseDashboardPage = () => (
     <div>
         <p>Esto es el copmonente de dashboard...</p>
+        <ExpenseList />
     </div>
 );
 
