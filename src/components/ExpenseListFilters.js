59211e79585d7adcef514fe69ca86e238b4fc076
sbenavid <sbenavid@gmail.com>
2018-10-23 17:54:14 -0500

    Agrego un select control para que ordenamiento por fecha/importe

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,14 +1,29 @@
 // manejo de los filtros 
 import React from 'react';
 import { connect } from 'react-redux';
-import { setTextFiler } from '../actions/filters';
+import { setTextFiler, sortByDate, sortByAmount } from '../actions/filters';
 
 const ExpenseListFilters = (props) => (
     <div>
-        <input type="text" value={props.filters.text} onChange={(e) => {
-            props.dispatch(setTextFiler(e.target.value));
-            console.log(e.target.value);
-        }}/>
+        <input 
+            type="text" 
+            value={props.filters.text} 
+            onChange={(e) => {
+                props.dispatch(setTextFiler(e.target.value));            
+            }}
+        />
+        <select 
+            value={props.filters.sortBy} 
+            onChange={(e) => {
+                if (e.target.value === 'date') {
+                    props.dispatch(sortByDate());
+                } else if (e.target.value === 'amount') {
+                    props.dispatch(sortByAmount());
+                }                
+        }}>
+            <option value="date">Fecha</option>
+            <option value="amount">Monto</option>
+        </select>
     </div>
 );
 
