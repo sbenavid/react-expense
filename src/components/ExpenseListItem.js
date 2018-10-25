c8b44e1663a600de496a06f3979419a8ae2e1813
sbenavid <sbenavid@gmail.com>
2018-10-25 09:12:21 -0500

    Agrego formato de moneda a los importes

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// biblioteca para formatear numeros: http://numeraljs.com/
+import numeral from 'numeral';
+// documentacion para moment: momentjs.com
+import moment from 'moment';
 
 // export a stateless functional component
 // description, amount, createdAt
@@ -8,7 +12,10 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>        
-        <p>{amount}-{createdAt}</p>
+        <p>
+        {numeral(amount).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMMM/DD/YYYY')}</p>
     </div>
 );
 
