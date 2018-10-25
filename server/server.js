826a8eb2171c14bbe48d49a8fd0c6a9b1c009698
sbenavid <sbenavid@gmail.com>
2018-10-24 23:57:33 -0500

    Asignacion dinamica del puerto para la app

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
@@ -11,6 +12,6 @@ app.get('*', (req,res) => {
 })
 
 // iniciar el server
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('Servidor iniciando');
 });
