2752220941d0735c2201fda07bfbdb18d864bf6a
sbenavid <sbenavid@gmail.com>
2018-10-26 00:16:48 -0500

    Agrego firebase en imports para conectrme con db externa

--- src/app.js
+++ src/app.js
@@ -10,25 +10,9 @@ import configStore from './store/configStore';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
+import './firebase/firebase';
 
 const store = configStore();
-
-// Agregar dos gastos
-store.dispatch(addExpense({ description: 'recibo del agua', amount:2500, createdAt:1000 }));
-store.dispatch(addExpense({ description: 'recibo gas natural', amount:5300 }));
-store.dispatch(addExpense({ description: 'renta', amount:46000, createdAt:12300 }));
-// filtro por "recibo"
-// store.dispatch(setTextFilter('recibo'));
-
-// funcion para hacer un timeout
-// setTimeout(() => {
-//     store.dispatch(setTextFilter(''));
-// },3000);
-
-// definir 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
