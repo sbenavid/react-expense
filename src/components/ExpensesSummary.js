330c65b5af07760bfec99d3c4c2dfdb92f2e8c3d
sbenavid <sbenavid@gmail.com>
2018-10-31 20:24:07 -0600

    Modificacion del summary y mejoras al boton

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,6 +1,7 @@
 // stateless functional component
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
@@ -9,8 +10,13 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formatterExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
     return (
-        <div>
-            <h1>Un total de {expenseCount} {expenseWord} sumando {formatterExpensesTotal} </h1>
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Un total de <span>{expenseCount}</span> {expenseWord} sumando <span>{formatterExpensesTotal}</span> </h1>
+            <div className="page-header__actions">
+                <Link className="button" to="/create">Nuevo Gasto</Link>
+            </div>
+          </div>            
         </div>
     );
 };
